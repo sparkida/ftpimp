a10d887d144c21779fe081a561b5dede25adb394
Nick Riley <nick@sparkida.com>
2015-09-11 09:40:03 -0700

    v2.0 stable, testing suite update

--- test/index.js
+++ test/index.js
@@ -3,179 +3,331 @@
  * (c) 2014 Nicholas Riley, Sparkida. All Rights Reserved.
  * @module test/index
  */
+var assert = require('assert'),
+	fs = require('fs'),
+	FTP = require('../'),
+	config = require('../config'),
+	ftp;
+//config.debug = false;
+describe('FTPimp', function () {
+	//TODO - change to main
+	before(function (done) {
+		this.timeout(10000);
+		/**create new FTP instance connection
+		 * and login are automated */
+		ftp = FTP.create(config, false);
+		ftp.connect(done);
+	});
 
-//TODO - change to main
-var FTP = require('../'),
-    config = require('./config'),
-    /**create new FTP instance connection
-     * and login are automated */
-    ftp = FTP.create(config, false),
-    Test = function (testName, args) {
-        var test = this;
-        test.testName = testName;
-        test.args = args;
-        test.pass = {
-            success: false
+	var testDir = 'foo' + String(new Date().getTime());
+	describe('ls#LIST: list remote files', function () {
+		it ('succeeds', function (done) {
+			ftp.ls('', function (err, res) {
+				assert(Array.isArray(res));
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.ls('somebadlookup', function (err, res) {
+				assert(err instanceof Error);
+				assert.equal(res, false);
+				done();
+			});
+		});
+	});
+	
+	describe('lsnames#NLST: name list of remote directory', function () {
+		it ('succeeds', function (done) {
+			ftp.lsnames('', function (err, res) {
+				assert(Array.isArray(res));
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.lsnames('somebadlookup', function (err, res) {
+				assert(err instanceof Error);
+				assert.equal(res, false);
+				done();
+			});
+		});
+	});
+	
+	describe('lsnames#NLST: name list of remote directory', function () {
+		it ('succeeds', function (done) {
+			ftp.lsnames('', function (err, res) {
+				assert(Array.isArray(res));
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.lsnames('somebadlookup', function (err, res) {
+				assert(err instanceof Error);
+				assert.equal(res, false);
+				done();
+			});
+		});
+	});
+	
+	describe('chdir#CWD: change working directory', function () {
+		it ('succeeds', function (done) {
+			ftp.chdir('', function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.chdir('somebadlookup', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
 
-        };
-        test.results = {
-            success: null
-        };
-                
-    },
-    cue = {},
-    cueIndex = [],
-    tests = [],
-    //pool of created test instances
-    pool = {};
+	describe('mkdir#MKD: make a remote directory', function () {
+		it ('succeeds', function (done) {
+			ftp.mkdir(testDir, function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.mkdir('', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
+	
+	describe('rmdir#RMD: remove remote directory', function () {
+		it ('succeeds', function (done) {
+			ftp.rmdir(testDir, function (err, res) {
+				assert(res);
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.rmdir('badDirectoryError', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
+	
+	describe('type#TYPE: set transfer types', function () {
+		it ('fails', function (done) {
+			ftp.type('badTypeError', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+		it ('changed type to image (binary data)', function (done) {
+			ftp.type('binary', function (err, res) {
+				assert(res);
+				done(err);
+			});
+		});
+		it.skip ('changed type to EBCDIC text(not available)', function (done) {
+			ftp.type('ebcdic', function (err, res) {
+				assert(res);
+				done(err);
+			});
+		});
+		it ('changed type to local format', function (done) {
+			ftp.type('local', function (err, res) {
+				assert(res);
+				done(err);
+			});
+		});
+		it ('changed type to ASCII text', function (done) {
+			ftp.type('ascii', function (err, res) {
+				assert(res);
+				done(err);
+			});
+		});
+	});
 
+	describe('setType: set transfer type based on file', function () {
+		it ('uses ASCII as default', function (done) {
+			ftp.setType('badTypeError', function (err, res) {
+				assert(ftp.currentType, 'ascii');
+				done();
+			});
+		});
+		it ('changes to binary for images', function (done) {
+			ftp.setType('test.png', function (err, res) {
+				assert(ftp.currentType, 'binary');
+				done();
+			});
+		});
+		it ('changes to ASCII for text', function (done) {
+			ftp.setType('index.js', function (err, res) {
+				assert(ftp.currentType, 'ascii');
+				done();
+			});
+		});
+	});
+	
+	
+	describe('put: transfer files to remote', function () {
+		it ('succeeds', function (done) {
+			ftp.put('index.js', function (err, res) {
+				assert.equal(res, 'index.js');
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.put('badFileError', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
+	
+	describe('rename#RNTO: rename to', function () {
+		it ('succeeds', function (done) {
+			ftp.rename(['index.js', 'ind.js'], function (err, res) {
+				assert(!!res);
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.rename(['missingFile', 'foo'], function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
+	
+	describe('get#RETR: retrieve remote file', function () {
+		it ('succeeds at getting ASCII text file', function (done) {
+			ftp.get('ind.js', function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
+		it ('succeeds at getting binary image file', function (done) {
+			ftp.type('binary', function (err, res) {
+				if(err) {
+					done(err);
+				} else {
+					ftp.get('test.png', function (err, res) {
+						if (err) {
+							done(err);
+						} else {
+							assert(typeof res === 'string');
+							done();
+						}
+					});
+				}
+			});
 
-Test.exe = function () {
-    var test = this,
-        errFunc, 
-        successFunc = function (err, data) {
-            console.log('test complete-----' + testName);
-            test.testSuccess.call(test, err, data);
-            ftp.emit('testComplete');
-        },
-        args = test.args,
-        testName = test.testName,
-        argLength = args.length;
+		});
+		it ('fails', function (done) {
+			ftp.get('fileNotFoundError', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
 
-    //for commands that don't have directly correlated errors
-    console.log('Executing Test: ' + testName);
-    if (undefined === args || argLength === 0) {
-        ftp[testName].call(ftp, successFunc);
-    } else {
-        if (argLength > 1) {
-            test.results.error = null;
-            test.pass.error = false;
-            successThenErr = function (err, data) {
-                test.testSuccess.call(test, err, data);
-                ftp[testName].call(ftp, args[1], function (err) { 
-                    test.testError.call(test, err);
-                    ftp.emit('testComplete');
-                });
-            };
-            //one for success, one for fail
-            ftp[testName].call(ftp, args[0], successThenErr);
-        } else {
-            ftp[testName].call(ftp, args[0], successFunc);
-        }
-        
-    }
-};
+	describe('save: retrieve remote file and save to local', function () {
+		it ('succeeds', function (done) {
+			ftp.save(['ind.js', 'ind-ftpimp-remote-saved.js'], function (err, res) {
+				assert(!!res);
+				fs.unlink('ind-ftpimp-remote-saved.js', function (delError, res) {
+					done(delError);
+				});
+			});
+		});
+		it ('fails', function (done) {
+			ftp.save(['missingFile', 'foo'], function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
 
+	describe('filemtime#MDTM: return the modification time of a remote file', function () {
+		it ('succeeds', function (done) {
+			ftp.filemtime('ind.js', function (err, res) {
+				assert(!isNaN(Number(res)));
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.filemtime('fileNotFoundError', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
 
-Test.run = function () {
-    if (cueIndex.length === 0) {
-        console.log('--- Testing Complete ---');
-        var i = 0,
-            str = '';
-        for (i; i < tests.length; i++) {
-            str = '\n' + tests[i] + '\n' + 'Success: ' 
-                    + (cue[tests[i]].pass.success ? 'pass'.green : 'fail'.red);
-            if (undefined !== cue[tests[i]].pass.error) {
-                str += '\nError: ' + (cue[tests[i]].pass.error ? 'pass'.green : 'fail'.red);
-            }
-            console.log(str);
-            //console.log(tests[i] + ': ', cue[tests[i]].results);
-        }
-        return;
-    }
+	describe('unlink#DELE: delete remote file', function () {
+		it ('succeeds', function (done) {
+			ftp.unlink('ind.js', function (err, res) {
+				assert.equal(res, 'ind.js');
+				done(err);
+			});
+		});
+		it ('fails', function (done) {
+			ftp.unlink('fileNotFoundError', function (err, res) {
+				assert(err instanceof Error);
+				assert(!res);
+				done();
+			});
+		});
+	});
 
-    var testName = cueIndex.shift(),
-        args = cue[testName];
-    console.log('Running Test: ' + testName);
-    pool[testName] = Test.exe.call(cue[testName]);
-};
+	
+	describe('General FTP commands', function () {
+		it ('ping#NOOP: do nothing, ping the remote server', function (done) {
+			ftp.ping(done);
+		});
 
+		it ('stat#STAT: get server status', function (done) {
+			ftp.stat(function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
 
-Test.create = function () { 
-    var i = 0,
-        args = [],
-        argLength = arguments.length,
-        testName,
-        id;
-    for(i; i < argLength; i++) {
-        args.push(arguments[i]);
-    }
-    testName = args.shift();
-    id = testName + '_' + String(new Date().getTime()) + Math.floor((Math.random() * 10000) + 1);    
-    //cue registered tests until connected
-    cue[id] = Test.build(testName, args);
-    cueIndex.push(id);
-    tests.push(id);
-    console.log('Test Created: ' + testName, args, '['+id.split('_').pop()+']');
-};
+		it ('root: changes to root directory', function (done) {
+			ftp.root(function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
 
+		it ('getcwd: gets current working directory', function (done) {
+			ftp.getcwd(function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
 
-Test.build = function (testName, args) {
-    return new Test(testName, args);
-};
+		it ('info#SYST: return system type', function (done) {
+			ftp.info(function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
+	});
 
-
-Test.prototype.testError = function (err) {
-    this.results.error = /*[this.testName, 'error',*/ err/*]*/;
-    this.pass.error = (null !== err);
-};
-
-
-Test.prototype.testSuccess = function (err, data) {
-    this.results.success = /*[this.testName, 'success', err,*/ data/*]*/;
-    this.pass.success = (null === err && null !== data);
-};
-
-Test.end = function () {
-    Test.create = function(){};
-    Test.run();
-    Test.run = function () {};
-};
-
-
-Test.buildTests = function () {
-
-    Test.create('ls', '', 'fooError');
-    Test.create('lsnames', '', 'fooError');
-    //test success, then error when applicable
-    Test.create('chdir', '', 'fooError');
-    //will fail at making root
-    var testDir = 'foo' + String(new Date().getTime());
-    Test.create('mkdir', testDir, '');
-    Test.create('ls', '', 'fooError');
-    Test.create('lsnames', '', 'fooError');
-    Test.create('rmdir', testDir, 'fooError');
-    Test.create('type', 'ascii');
-    Test.create('put', 'index.js', 'foo');
-    Test.create('rename', ['index.js', 'ind.js'], ['index.js', 'foo.js']);
-    Test.create('type', 'binary');
-    Test.create('get', 'ind.js', 'fooError');
-    Test.create('get', 'test.png');
-    Test.create('put', 'index.js');
-    Test.create('save', ['ind.js', 'saved_ind.js'], 'fooError');
-    Test.create('filemtime', 'ind.js', 'fooError');
-    Test.create('unlink', 'ind.js', 'fooError');
-    Test.create('run', 'NOOP');
-
-    //stops the test immediately
-    //Test.end();
-
-    Test.create('root');
-    Test.create('getcwd');
-    //Test.create('site');
-    Test.create('stat');
-    Test.create('info');
-    Test.create('ping');
-
-
-    Test.create('quit');
-    Test.run();
-};
-
-
-
-
-ftp.connect(Test.buildTests);
-ftp.on('testComplete', Test.run);
-
-
+	describe('it closes connection to remote', function () {
+		it ('quit#QUIT: terminates connection to remote', function (done) {
+			ftp.quit(function (err, res) {
+				assert(typeof res === 'string');
+				done(err);
+			});
+		});
+	});
+});
